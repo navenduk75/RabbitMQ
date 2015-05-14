42dbf6c09a7bddc161513b04a355b4929ff1d0ec
navenduk75 <navenduk75@yahoo.com>
2015-05-13 21:54:18 -0400

    1st committ

---
+++ RabbitMQ/Send.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace RabbitMQ
+{
+    class Send
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello",false,false,false,null);
+
+                    string message = "Hello World";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("","hello",null,body);
+                    Console.WriteLine("[x] sent {0}",message);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
